4ef750b8221e0b7c32c2636d3628838020eb4a80
anapedra <anapedra.mil@gmail.com>
2024-04-26 05:14:12 -0300

    Client

---
+++ ContaBanco/src/main/java/com/anapedra/contabanco/Client.java
@@ -0,0 +1,52 @@
+package com.anapedra.contabanco;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_client")
+public class Client implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String name;
+    private String password;
+
+    public Client() {
+
+    }
+
+    public Client(Long id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
