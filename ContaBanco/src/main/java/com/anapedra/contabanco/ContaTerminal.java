ecdfee60acfc8c9d4744540cf9f6da080d7273a9
anapedra <anapedra.mil@gmail.com>
2024-04-26 04:45:55 -0300

    Project ContaBancaria Created

---
+++ ContaBanco/src/main/java/com/anapedra/contabanco/ContaTerminal.java
@@ -0,0 +1,21 @@
+package com.anapedra.contabanco;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaTerminal implements CommandLineRunner {
+
+    public static void main(String[] args) {
+            SpringApplication.run(ContaTerminal.class, args);
+
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        
+
+    }
+}
