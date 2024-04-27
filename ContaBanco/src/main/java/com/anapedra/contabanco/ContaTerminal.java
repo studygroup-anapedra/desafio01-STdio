7785de65d83ac4cebf0fa56f8be292ada859cbb6
anapedra <anapedra.mil@gmail.com>
2024-04-26 22:04:47 -0300

    Fix:ContaTerminal

--- ContaBanco/src/main/java/com/anapedra/contabanco/ContaTerminal.java
+++ ContaBanco/src/main/java/com/anapedra/contabanco/ContaTerminal.java
@@ -1,11 +1,14 @@
 package com.anapedra.contabanco;
 
+import com.anapedra.contabanco.model.Account;
 import com.anapedra.contabanco.model.Client;
+import com.anapedra.contabanco.repositories.AccountRepository;
 import com.anapedra.contabanco.repositories.ClientRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 @SpringBootApplication
@@ -13,8 +16,10 @@ public class ContaTerminal implements CommandLineRunner {
     private static Scanner sc = new Scanner(System.in);
 
     private final ClientRepository clientRepository;
-    public ContaTerminal(ClientRepository clientRepository) {
+    private final AccountRepository accountRepository;
+    public ContaTerminal(ClientRepository clientRepository, AccountRepository accountRepository) {
         this.clientRepository = clientRepository;
+        this.accountRepository = accountRepository;
     }
 
     public static void main(String[] args) {
@@ -25,6 +30,7 @@ public class ContaTerminal implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
          Client client = new Client();
+         Account account = new Account();
         System.out.println();
 
         System.out.println("Digite seu nome: ");
@@ -36,8 +42,41 @@ public class ContaTerminal implements CommandLineRunner {
         System.out.println();
 
 
-        System.out.println("Nome: " + client.getName());
-        System.out.println("CPF: " + client.getCpf());
+        System.out.println("Olá "+ client.getName() + ", está indo bem! Continue!" +
+                "\nVocê deverá digitar os números da  conta e agencia sugeridos e em segida crie sua senha.\n" +
+                "\nATENÇÃO: SUA SENHA DEVERÁ SER DE USO EXCLISIVO SEU! PARA SUA SEGURAÇA,NÃO COMPARTILHE INFORMAÇÕES SENSIVEIS DA SUA CONTA.!");
+
+        System.out.println("\nDigite 067-8 para o número da agencia: ");
+        account.setAgency(sc.nextLine());
+
+        System.out.println("Digite  1021 para o número da conta: ");
+        account.setNumber(sc.nextInt());
+
+        System.out.println("Vamos lá, já está quase acabando! Para concluir digite sua senha: ");
+         account.setPassword(sc.nextLine());
+
+        account.setClient(client);
+        account.setBalance(BigDecimal.ZERO);
+        accountRepository.save(account);
+
+        System.out.println("\nBanco Santander" +
+                "\nTitular da conta...................."+ account.getClient().getName() +
+                "\nCpf do titular...................."+ account.getClient().getCpf() +
+                "\nNumero da conta..................."+ account.getNumber() +
+                "\nNúmero da agencia................."+ account.getAgency() +
+                "\nSaldo da conta...................."+ account.getBalance()
+        );
+        System.out.println("\nÓtimo, "+account.getClient().getName()+ " Vocẽ já é uma(o) Santander! Parabéns!" +
+                "\nAgora você prescisa fazer um deposito de no mínimo $100\nEscolha a opção depositar: ");
+
+
+
+
+
+
+
+
+
 
 
 
